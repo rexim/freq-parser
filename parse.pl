c6e4f58c73e679047871b40c0ad54bcd9371dab0
rexim <reximkut@gmail.com>
2014-08-20 16:13:49 +0700

    Added some notes about properties of the logs.

--- parse.pl
+++ parse.pl
@@ -5,19 +5,20 @@ use warnings;
 
 use Data::Dumper;
 
-my $message_type_mapping = {
-    'mn' => 'Regular message',
-    'mne' => '/me message',
-    'mj' => 'Join message',
-    'ml' => 'Leave message',
-    'roomcsubject' => 'Subject'
-};
+# mn properties:
+# * just a message - ($msg1 =~ /&lt;(.*?)&gt;/) && ($msg2 =~ /(.*?)/)
 
-# Subtypes of ml:
-# * just leaved  - ($type eq 'ml') && $msg1 =~ /(.+) вышел из конференции(: (.*))?$/ && ($msg2 eq "")
-# * kicked       - ($type eq 'ml') && $msg1 =~ /(.+) выгнали из конференции(: (.*))?$/ && ($msg2 eq "")
-# * banned       - ($type eq 'ml') && $msg1 =~ /(.+) запретили входить в конференцию(: (.*))?$/ && ($msg2 eq "")
-# * changed nick - ($type eq 'ml') && $msg1 =~ /(.+) сменил ник на (.*)?$/ && ($msg2 eq "")
+# mne properties:
+# * /me message - $msg1 =~ /\* (.+) (.+)/
+
+# ml properties:
+# * just left    - $msg1 =~ /(.+) вышел из конференции(: (.*))?$/ && ($msg2 eq "")
+# * kicked       - $msg1 =~ /(.+) выгнали из конференции(: (.*))?$/ && ($msg2 eq "")
+# * banned       - $msg1 =~ /(.+) запретили входить в конференцию(: (.*))?$/ && ($msg2 eq "")
+# * changed nick - $msg1 =~ /(.+) сменил ник на (.*)?$/ && ($msg2 eq "")
+
+# mj properties:
+# * just joined - ($msg1 =~ /(.+) зашёл в конференцию$/) && ($msg2 eq "")
 
 sub iterate_messages_in_file {
     my ($file_name, $action) = @_;
@@ -32,6 +33,13 @@ sub iterate_messages_in_file {
 
 sub count_types_in_file {
     my ($file_name, $types) = @_;
+    my $message_type_mapping = {
+        'mn' => 'Regular message',
+        'mne' => '/me message',
+        'mj' => 'Join message',
+        'ml' => 'Leave message',
+        'roomcsubject' => 'Subject'
+    };
     iterate_messages_in_file $file_name, sub {
         my ($time, $raw_type, $msg1, $msg2) = @_;
         my $type = $message_type_mapping->{$raw_type};
